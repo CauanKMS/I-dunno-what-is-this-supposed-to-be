5c56e3407f4fa6cea9b35b0cec8de26e3668ae03
Cauan.KMS <cauan.kms@gmail.com>
2018-03-01 20:38:57 -0300

    Changed backgroundColor and removed unused lines
    Changed backgroundColor to a slightly more pink color.

--- JStuff/jscript.js
+++ JStuff/jscript.js
@@ -10,10 +10,7 @@ function vaporWave(){
 	var bodyStyle = document.body.style;
 	var btnSomethingStyle = btnSomething.style;
 
-    	//var audio = new Audio('files/macintoshplus.mp3');
-    	//audio.play();
-
-	bodyStyle.backgroundColor = "#f000f0";
+    bodyStyle.backgroundColor = "#FE4BA0";
 	bodyStyle.fontFamily = "VCR";
 
 	btnSomethingStyle.backgroundColor = "#ffffff";
